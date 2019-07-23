49bea91da070b93c8e6335ae50b4d565b53508e1
rubeus77 <rubeus77@gmail.com>
2019-07-23 21:58:08 +0200

    added imports

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import Steps from "./components/steps";
 import About from "./components/about";
 import Fundations from "./components/fundations-list";
 import Contact from "./components/contact";
+import * as Scroll from 'react-scroll';
+import {
+  HashRouter,
+  Route,
+  Link,
+  Switch,
+  NavLink,
+} from 'react-router-dom';
+
 import './App.scss';
 
 class App extends Component{
