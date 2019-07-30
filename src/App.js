c3cffdc3af1a79e885363bf501cfe3dfa7e17f68
Rafal Nowak <rubeus77@gmail.com>
2019-07-30 10:08:04 +0200

    Corection of logic in App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
-import PageHeader from "./components/page-header";
-//import YellowBar from "./components/yellow-bar";
-//import Steps from "./components/steps";
-import About from "./components/about";
-//import Fundations from "./components/fundations-list";
-//import Contact from "./components/contact";
-// import * as Scroll from 'react-scroll';
-// import {
-//   HashRouter,
-//   Route,
-//   Link,
-//   Switch,
-//   NavLink,
-// } from 'react-router-dom';
 
 import "./App.scss";
 
 class App extends Component {
+  state = {
+    isSession: false
+  }
   render() {
     return (
       <React.Fragment>
