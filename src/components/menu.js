c8ec89dfa901a03049d3c9c117c59f7ed05d5e68
Rafal Nowak <rubeus77@gmail.com>
2019-08-02 11:13:56 +0200

    component Steps finished

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
+import LogIn from './log-in';
 
 class Menu extends Component{
     render(){
         return(
-            <>
-            </>
+            <nav>
+                <LogIn log={this.props.log}/>
+            </nav>
         )
     }
 }
