711b9ab144dd2e1267a14c574947f91d9f3e4cc6
rubeus77 <rubeus77@gmail.com>
2019-08-07 14:39:59 +0200

    Menu conponent with react-scroll

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,30 @@
 import React, {Component} from 'react';
 import LogIn from './log-in';
+import { Link } from 'react-scroll'
 
 class Menu extends Component{
+    state ={
+        menu: null
+    }
+    componentDidMount(){
+        fetch("http://localhost:3001/menu")
+        .then( resp => resp.json())
+        .then( data => {
+            this.setState({
+                menu: data
+            })
+        })
+    }
     render(){
         return(
-            <nav>
+            <nav className="Menu">
                 <LogIn log={this.props.log}/>
+                {this.state.menu!==null &&(
+                    <ul>
+                        {this.state.menu.map( (elem, ind) => <li key={ind}><Link to={elem.to}>{elem.name}</Link></li>)}
+                    </ul>
+                )}
+                
             </nav>
         )
     }
